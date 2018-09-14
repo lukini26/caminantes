ae8ec1ca1dac62eff2f0de18b6d907e8b09452df
lukini <lucas_masciotra@hotmail.com>
2018-09-14 13:00:13 -0300

    lindo lindo lindowww

--- routes/router.js
+++ routes/router.js
@@ -96,7 +96,7 @@ router.get('/profile', function (req, res, next) {
           err.status = 400; 
           return next(err);
         } else {
-          return res.render('profile' , { uteam: user.team , uemail: user.email , uname: user.username , uid: user.pokeid} )
+          return res.render('profile' , { team: user.team , uemail: user.email , uname: user.username , uid: user.pokeid} )
           
           //('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>'
         //+'<br><a type="button" href="/list">Listado de Usuarios</a>')
